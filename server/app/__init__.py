ae2f636e898bfc072a2a03bf77aecf95f78ba9d2
Roman Khabayev <supernaturalmlady@gmail.com>
2023-11-17 18:47:44 +0600

    Project creation and setup

---
+++ server/app/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+def create_app():
+  app = Flask(__name__)
+
+  app.config.from_pyfile('../config.py')
+
+  from app import routes
+  app.register_blueprint(routes.bp)
+
+  return app
